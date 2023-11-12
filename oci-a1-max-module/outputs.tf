8c02cf19be3e3a32bf236eaa0fcab539a7db7741
Joe Harrison <53116754+sudo-kraken@users.noreply.github.com>
2023-11-12 10:16:06 +0000

    Update outputs.tf
    added additional outputs and descriptions

--- oci-a1-max-module/outputs.tf
+++ oci-a1-max-module/outputs.tf
@@ -1,12 +1,14 @@
-# Output the "list" of all availability domains.
-output "all-availability-domains-in-your-tenancy" {
-  value = data.oci_identity_availability_domains.ads.availability_domains
-}
-
-output "compartment-name" {
-  value = oci_identity_compartment.oci_a1_max.name
-}
-
 output "public-ip-ampere-instance" {
+  description = "The public IP addresse assigned to the A2 instance"
   value = oci_core_instance.vm_instance_ampere.public_ip
+}
+
+output "private-ip-ampere-instance" {
+  description = "The private IP addresse assigned to the A2 instance"
+  value = oci_core_instance.vm_instance_ampere.private_ip
+}
+
+output "instance-id-ampere-instance" {
+  description = "The OCID of the A1 instance"
+  value = oci_core_instance.vm_instance_ampere.id
 }
