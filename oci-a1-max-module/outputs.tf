fcc30e3e8c921db04b31357a3d11f8118d4c2806
Joe Harrison <joe@j-harrison.co.uk>
2026-06-16 10:18:50 +0100

    Fix descriptions for A1 instance IP outputs

--- oci-a1-max-module/outputs.tf
+++ oci-a1-max-module/outputs.tf
@@ -1,10 +1,10 @@
 output "public-ip-ampere-instance" {
-  description = "The public IP addresse assigned to the A2 instance"
+  description = "The public IP address assigned to the A1 instance"
   value = oci_core_instance.vm_instance_ampere.public_ip
 }
 
 output "private-ip-ampere-instance" {
-  description = "The private IP addresse assigned to the A2 instance"
+  description = "The private IP address assigned to the A1 instance"
   value = oci_core_instance.vm_instance_ampere.private_ip
 }
 
